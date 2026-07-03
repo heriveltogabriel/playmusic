ecf53b19ad8c88e3079da96f8be89b6a2cb1092b
MacbookPro <macbookpro@192.168.68.128>
2026-07-02 22:07:57 -0300

    chore: exclude collection.db from deploy and bump version to 3.1.1

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,7 @@ tar --exclude="static-backup-v1" \
     --exclude="ssh" \
     --exclude="data" \
     --exclude="arquivos_sensiveis.zip" \
+    --exclude="collection.db" \
     -cf - . | ssh -o StrictHostKeyChecking=no -i ssh/ssh-key-2026-05-26.key opc@150.136.207.62 "tar -C /home/opc/vinyl_display -xf -"
 
 echo "🧹 Cleaning up metadata files on the remote server..."
